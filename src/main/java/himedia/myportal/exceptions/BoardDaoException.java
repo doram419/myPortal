3f779d0fb11d3d15bb5d164a4b40c2e189e6bd91
JiHwan Kim <57897955+doram419@users.noreply.github.com>
2024-06-26 11:01:39 +0900

    create BoardDaoException

---
+++ src/main/java/himedia/myportal/exceptions/BoardDaoException.java
@@ -0,0 +1,27 @@
+package himedia.myportal.exceptions;
+
+import himedia.myportal.repositories.vo.BoardVo;
+
+public class BoardDaoException extends RuntimeException{
+	private BoardVo boardVo = null;
+	
+	public BoardDaoException() {
+	}
+	
+	public BoardDaoException(String message) {
+		super(message);
+	}
+
+	public BoardDaoException(String message, BoardVo vo) {
+		super(message);
+		boardVo = vo;
+	}
+
+	public BoardVo getBoardVo() {
+		return boardVo;
+	}
+
+	public void setBoardVo(BoardVo boardVo) {
+		this.boardVo = boardVo;
+	}
+}
