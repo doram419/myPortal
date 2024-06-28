c4c25d1fe48ab267f983e5f6bfe4dc816dadba4e
JiHwan Kim <57897955+doram419@users.noreply.github.com>
2024-06-28 11:54:12 +0900

    interceptor mapping

--- src/main/java/himedia/myportal/interceptors/MyInterceptor.java
+++ src/main/java/himedia/myportal/interceptors/MyInterceptor.java
@@ -2,6 +2,7 @@ package himedia.myportal.interceptors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -28,9 +29,21 @@ public class MyInterceptor implements HandlerInterceptor{
 
 	// URL 처리 핸들러 작동 후
 	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+	public void postHandle(HttpServletRequest request, 
+			HttpServletResponse response, 
+			Object handler,
 			ModelAndView modelAndView) throws Exception {
 		logger.debug("MyInterceptor::postHandle()");
+		// handler 확인
+		if(handler instanceof HandlerMethod) {
+			HandlerMethod handlerMethod = (HandlerMethod) handler;
+			// 호출된 컨트롤러 메서드 정보 추출
+			String controllerName = handlerMethod.getBeanType().getName();
+			// 호출된 메서드 이름 추출
+			String methodName = handlerMethod.getMethod().getName();
+			
+			logger.debug("Controller : " + controllerName + ", Method : " + methodName);
+		}
 
 		HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
 	}
