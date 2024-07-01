a84211842262909afbca6765b2c3032703660cb8
JiHwan Kim <57897955+doram419@users.noreply.github.com>
2024-07-01 18:32:54 +0900

    create attachDao, attachDaoImpl

--- src/main/java/himedia/myportal/repositories/dao/AttachDao.java
+++ src/main/java/himedia/myportal/repositories/dao/AttachDao.java
@@ -1,5 +1,11 @@
 package himedia.myportal.repositories.dao;
 
+import java.util.List;
+
+import himedia.myportal.repositories.vo.AttachVo;
+
 public interface AttachDao {
-	
+	public int insertAttach(AttachVo vo, Long postNo);
+	public List<AttachVo> getAttaches();
+	public int deleteAttaches(Long postNo);
 }
---
+++ src/main/java/himedia/myportal/repositories/dao/AttachDaoImpl.java
@@ -0,0 +1,27 @@
+package himedia.myportal.repositories.dao;
+
+import java.util.List;
+
+import himedia.myportal.repositories.vo.AttachVo;
+
+public class AttachDaoImpl implements AttachDao{
+
+	@Override
+	public int insertAttach(AttachVo vo, Long postNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<AttachVo> getAttaches() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int deleteAttaches(Long postNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
